28a72f90d7ee3a244a37f1f468ecf351bcdc8e85
kv4849-lab <kv4849@srmist.edu.in>
2026-03-03 10:23:12 +0530

    UC1: printstatements World message

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,20 @@
 /**
- * OOPSBannerApp
+ * OOPSBannerApp - UC2
+ * Displays OOPS in banner format using only print statements
  * @author YourName
- * @version 1.0
+ * @version 2.0
  */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****  " + "  *****  " + " ******** " + " ******** ");
+        System.out.println(" *     * " + " *     * " + " *      * " + " *        ");
+        System.out.println(" *     * " + " *     * " + " *      * " + " *        ");
+        System.out.println(" *     * " + " *     * " + " ******** " + "  ******  ");
+        System.out.println(" *     * " + " *     * " + " *        " + "        * ");
+        System.out.println(" *     * " + " *     * " + " *        " + "        * ");
+        System.out.println("  *****  " + "  *****  " + " *        " + "  ******  ");
     }
 }
